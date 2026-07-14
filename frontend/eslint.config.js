08eba599b0aaa223093eba500c0d0d5b4b547542
Spencer <snook2945@gmail.com>
2026-07-14 19:19:27 -0400

    Add minimal Google login UI

---
+++ frontend/eslint.config.js
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist'],
+  },
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  },
+);
